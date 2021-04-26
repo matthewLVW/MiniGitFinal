ab506f77ad921ba6f0695553464faa5ef76a8591
Bach Nguyen <73509508+Bach2324@users.noreply.github.com>
2021-04-26 14:57:02 -0600

    Update miniGit.hpp

--- miniGit.hpp
+++ miniGit.hpp
@@ -2,23 +2,6 @@
 
 using namespace std;
 
-//Commmit logic
-/*
-add file
-When user commit, check each file to look for difference
-
-if a file is found to not be the same, create a new version of that file and increment the version number
-else, do nothing to the file name and do not add the file into .miniGit
-
-when commit, dynamically allocate doubly LL node
-also need to dynamically allocate singly LL node
-    set node data to equal the name and commit version
-increment commit number
-set previous and next pointer
-copy all of singly LL node over
-
-*/
-
 struct singlyNode
 {
     string fileName;
@@ -32,7 +15,6 @@ struct doublyNode
     doublyNode * previous;
     doublyNode * next;
 };
-
 class miniGit
 {
     private:
@@ -43,7 +25,7 @@ class miniGit
 
         miniGit();
         ~miniGit();
-        void init();
+        void init(doublyNode *dNode);
         doublyNode *currCommit(int commit);
         bool checkFile(string file, int commit);
         void addFile(doublyNode *Dnode, string fileName);
@@ -52,5 +34,5 @@ class miniGit
         void copyList(doublyNode *Dnode, singlyNode *OG);
         void commit(int num_commit);
         doublyNode *addDDnode(int incrementCommit);
-        
+        void checkout(int commitnumber);
 };
